c56ea12901ed3e610db4fabbcf2fcf9133bd41c9
saadetsimsek <saadetsimsek13@gmail.com>
2024-11-05 21:47:47 +0300

    Initial Commit

---
+++ CustomTextField/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTextField
+//
+//  Created by Saadet Şimşek on 05/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CustomTextFieldTests/CustomTextFieldTests.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTextFieldTests.swift
+//  CustomTextFieldTests
+//
+//  Created by Saadet Şimşek on 05/11/2024.
+//
+
+import Testing
+@testable import CustomTextField
+
+struct CustomTextFieldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
